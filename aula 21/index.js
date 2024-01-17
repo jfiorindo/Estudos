667b4545c837a84be2b7849174b577eabcfb8347
joão <joaopedrofiorindopinto@gmail.com>
2024-01-17 14:02:37 -0300

    aulas 20/21

---
+++ aula 21/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual que
+< menor que
+<= menor igual que
+== igualdade
+=== igualdade estrita
+!= diferente
+!=== diferente estrita
+
+*/
+const comp = 10 > 5;
+console.log(comp);
--- aula19/index.js
+++ aula19/index.js
@@ -1,3 +1,5 @@
 /*
 primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+
+referência (mutaveis) - array, object, function
 */
