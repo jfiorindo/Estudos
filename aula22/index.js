bfac08e36ddf90f2e155e2d641e631ef2be777af
joão <joaopedrofiorindopinto@gmail.com>
2024-01-23 18:30:35 -0300

    aulas 22/23

---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+/* 
+operadores lógicos
+ &&  -> AND -> se não for tudo true vai retornar false
+ || -> OR -> se não for tudo false vai retornar true
+ ! -> NOT ->
+*/
+
+const expressaoAnd =  true && true;
+const expressaoOr = false || false;
+const expressaoNot = !false
+console.log(expressaoNot);
---
+++ aula23/index.js
@@ -0,0 +1,30 @@
+/*
+FALSY
+*false
+0
+'' "" ``
+null / undefined 
+NaN
+
+function falaOi () { 
+    return 'oi';
+}
+
+let VaiExecutar = 'joãozinho'
+
+console.log(VaiExecutar && falaOi ());
+
+const corUsuário = 'vermelho';
+const corPadrao = corUsuário || 'preto';
+
+console.log(corPadrao);*/
+
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e);
+
+
