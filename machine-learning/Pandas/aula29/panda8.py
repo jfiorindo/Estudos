29eebee467c28c2452eb03d38e9b07929b37142d
joão <joaopedrofiorindopinto@gmail.com>
2024-04-25 14:02:03 -0300

    Teste

--- machine-learning/Pandas/aula29/panda8.py
+++ machine-learning/Pandas/aula29/panda8.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt #biblioteca para Gráficos
 
 tabela = pd.read_csv('C:/Users/João/Desktop/py/athlete_events.csv')
 
-tabela2 = tabela.hist(column = 'Weight', bins = 100)
+tabela2 = tabela.hist(column = 'Age', bins = 100)
 plt.show()
 #faz gráficos!!!!!!!
